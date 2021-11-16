57587ddf4191a6c09f211a0858518c0d393b2192
LittleGeorgeWatchingStar <quanzhangubc2019@gmail.com>
2021-11-16 11:33:15 -0800

    fix splash screen and add register func

--- src/api/client.js
+++ src/api/client.js
@@ -6,6 +6,12 @@ const apiClient = create({
   // baseURL: "https://moshbucket1.s3.us-east-2.amazonaws.com/api",
 });
 
+apiClient.addAsyncRequestTransform(async (request) => {
+  const authToken = await authStorage.getToken();
+  if (!authToken) return;
+  request.headers["x-auth-token"] = authToken;
+});
+
 const get = apiClient.get;
 apiClient.get = async (url, params, axiosConfig) => {
   const response = await get(url, params, axiosConfig);
--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Image, View, Text } from "react-native";
 import * as Yup from "yup";
 
@@ -6,10 +6,16 @@ import colors from "../config/colors";
 import Screen from "../components/Screen";
 import logoRed from "../../assets/logo-red.png";
 import {
+  ErrorMessage,
   AppForm as Form,
   AppFormField as FormField,
   SubmitButton,
 } from "../components/forms";
+import usersApi from "../api/users";
+import authApi from "../api/auth";
+import useAuth from "../auth/useAuth";
+import useApi from "../hooks/useApi";
+import ActivityIndicator from "../components/ActivityIndicator";
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required().label("Name"),
@@ -18,6 +24,30 @@ const validationSchema = Yup.object().shape({
 });
 
 function RegisterScreen() {
+  const registerApi = useApi(usersApi.register);
+  const loginApi = useApi(authApi.login);
+  const auth = useAuth();
+  const [error, setError] = useState();
+
+  const handleSubmit = async (userInfo) => {
+    const result = await registerApi.request(userInfo);
+
+    if (!result.ok) {
+      if (result.data) setError(result.data.error);
+      else {
+        setError("An unexpected error occurred.");
+        console.log(result);
+      }
+      return;
+    }
+
+    const { data: authToken } = await loginApi.request(
+      userInfo.email,
+      userInfo.password
+    );
+    auth.logIn(authToken);
+  };
+
   return (
     <Screen style={styles.container}>
       <View style={styles.logoContainer}>
@@ -29,9 +59,10 @@ function RegisterScreen() {
       <View style={styles.inputContainer}>
         <Form
           initialValues={{ name: "", email: "", password: "" }}
-          onSubmit={(values) => console.log(values)}
+          onSubmit={handleSubmit}
           validationSchema={validationSchema}
         >
+          <ErrorMessage error={error} visible={error} />
           <FormField
             autoCorrect={false}
             icon="account"
