abe252cdaf0130f859683f4ae70287926c6e810e
LittleGeorgeWatchingStar <quanzhangubc2019@gmail.com>
2021-11-16 10:35:09 -0800

    change app.js

--- App.js
+++ App.js
@@ -1,41 +1,37 @@
-import React, { useEffect, useState } from "react";
-import { Text, View, Button, Image } from "react-native";
-import WelcomeScreen from "./src/screens/WelcomeScreen";
-import ViewImageScreen from "./src/screens/ViewImageScreen";
+import React, { useState } from "react";
 import { NativeBaseProvider } from "native-base";
-import ListingDetailsScreen from "./src/screens/ListingDetailsScreen";
-import AccountScreen from "./src/screens/AccountScreen";
-import ListingsScreen from "./src/screens/ListingsScreen";
-import MessagesScreen from "./src/screens/MessagesScreen";
-import Screen from "./src/components/Screen";
-import AppPicker from "./src/components/Picker";
-import AppTextInput from "./src/components/TextInput";
-import LoginScreen from "./src/screens/LoginScreen";
-import ListingEditScreen from "./src/screens/ListingEditScreen";
-import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
-import { Camera } from "expo-camera";
-import { Loaction } from "expo-location";
-import ImageInput from "./src/components/ImageInput";
-import ImageInputList from "./src/components/ImageInputList";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { AppLoading } from "expo";
 
 import AppNavigator from "./src/navigation/AppNavigator";
 import navigationTheme from "./src/navigation/navigationTheme";
 import AuthNavigator from "./src/navigation/AuthNavigator";
 import OfflineNotice from "./src/components/OfflineNotice";
+import AuthContext from "./app/auth/context";
+import authStorage from "./app/auth/storage";
 
 export default function App() {
+  const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
+
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
+  };
+
+  if (!isReady)
+    return (
+      <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} />
+    );
+
   return (
     <NativeBaseProvider>
-      <OfflineNotice />
-      <NavigationContainer theme={navigationTheme}>
-
-        <AppNavigator />
-        {/* <AuthNavigator /> */}
-      </NavigationContainer>
+      <AuthContext.Provider value={{ user, setUser }}>
+        <OfflineNotice />
+        <NavigationContainer theme={navigationTheme}>
+          {user ? <AppNavigator /> : <AuthNavigator />}
+        </NavigationContainer>
+      </AuthContext.Provider>
     </NativeBaseProvider>
   );
 }
